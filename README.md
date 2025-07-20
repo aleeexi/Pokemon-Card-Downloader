9622c352f8a4c1d00fea0730a4a603182fbbc845
aleeexi <l0g8p@ugrad.cs.ubc.ca>
2025-07-20 02:05:16 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pokemon Card Downloader
+
+Downloads images of Pokemon cards of different rarities (Common, Uncommon, Rare, Double Rare, Ultra Rare) from [this website](https://pkmncards.com) with specific file name formatting (Name-EnergyType-IndexOnPage.jpg).
+
+### How to Use:
+[IN PROGRESS]
+
+### Technology Used:
+**Languages:** Python
+**Other:** Selenium (Chrome Web Driver)
---
+++ src/ultra-rare.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+import requests
+import time
+import urllib
+
+options = Options()
+options.headless = False # Set to True to enable headless mode (no window pop-up)
+
+driver = webdriver.Chrome(options = options)
+# Navigate webdriver to database of Ultra Rare Pokemon cards
+driver.get("https://pkmncards.com/?s=type%3Apokemon+rarity%3Aultra-rare&sort=abc&ord=auto&display=full")
+
+# Navigate page of Pokemon cards and download all card images with name-energy-index.jpg file name formatting
+def navigate_page():
+    # Load images on page
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.entry-content")))
+
+    # Find card content elements
+    cards = driver.find_elements(By.CSS_SELECTOR, "div.entry-content")
+
+    for i, card in enumerate(cards):
+        # Get name of Pokemon in card
+        name_element = card.find_element(By.CSS_SELECTOR, "span.pokemon")
+        name = name_element.text
+
+        # Get energy type of Pokemon in card
+        energy_element = card.find_element(By.CSS_SELECTOR, "abbr.ptcg-font.ptcg-symbol-name")
+        energy = energy_element.get_attribute("title")
+
+        # Download Pokemon card image
+        # Image download code from https://plainenglish.io/blog/web-scraping-images-with-python-and-selenium-792e452abd70
+        image = card.find_element(By.CSS_SELECTOR, "img.card-image")
+        url = image.get_attribute("src")
+        urllib.request.urlretrieve(str(url), "images/ultra-rare/" + name + "-" + energy + "-{}.jpg".format(i))
+    
+    # Navigate to next page using right arrow key shortcut
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.RIGHT)
+    actions.perform()
+
+# Navigate all search result pages of Ultra Rare Pokemon cards
+while True:
+    navigate_page()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.after-content")))
+    try:
+        next_link = driver.find_element(By.CSS_SELECTOR, "li.next-link")
+    except:
+        break
+driver.quit()
